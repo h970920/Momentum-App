9a64e86306ad78440a4da24dca8c2ca1b013afaf
h970920 <h970920@naver.com>
2023-12-13 10:39:58 +0900

    2023-12-13

--- app.js
+++ app.js
@@ -7,14 +7,18 @@ const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
-  loginForm.classList.add("hidden");
-  greeting.innerText = "Hello! " + username;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  paintGreeting(username);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
+
+function paintGreeting(username) {
+  greeting.innerText = "Hello! " + username;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
@@ -22,6 +26,5 @@ if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-  greeting.classList.remove(HIDDEN_CLASSNAME);
-  greeting.innerText = 
+  paintGreeting(savedUsername);
 }
